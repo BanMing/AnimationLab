4cb85e708be28e177a1f5804830d7a3850c33dba
BanMing <ban-ming@foxmail.com>
2021-07-30 00:35:28 +0800

    add dx12 initialize

--- CharactorAnimation/Chapter2/Application.cpp
+++ CharactorAnimation/Chapter2/Application.cpp
@@ -1,13 +1,5 @@
 #include "Application.h"
 
-
-const int WINDOW_WIDTH = 800;
-const int WINDOW_HEIGHT = 600;
-
-// Global Variables:
-LPCWSTR g_Title = L"Character Animation with Direct3D: Example 2.1"; // The title bar text
-LPCWSTR g_WindowClass = L"D3DWND"; // the main window class name
-
 Application::Application()
 {
 }
@@ -15,6 +7,8 @@ Application::Application()
 Application::~Application()
 {
 }
+
+#pragma region Initialize
 
 HRESULT Application::Init(HINSTANCE hInstance, bool windowed, WNDPROC wndProc)
 {
@@ -33,11 +27,11 @@ HRESULT Application::Init(HINSTANCE hInstance, bool windowed, WNDPROC wndProc)
 
 bool Application::InitMainWindow(HINSTANCE hInstance, WNDPROC wndProc)
 {
-	WNDCLASS wc;                        // Create Window Class
-	wc.style = CS_HREDRAW | CS_VREDRAW; // Window Style
-	wc.lpfnWndProc = wndProc;			// Window Event Procedure (more on this later)
-	wc.hInstance = hInstance;			// The Application Instance
-	wc.lpszClassName = g_WindowClass;	// The Window Class Name
+	WNDCLASS wc;							// Create Window Class
+	wc.style = CS_HREDRAW | CS_VREDRAW;		// Window Style
+	wc.lpfnWndProc = wndProc;				// Window Event Procedure (more on this later)
+	wc.hInstance = hInstance;				// The Application Instance
+	wc.lpszClassName = m_windowClassName.c_str();	// The Window Class Name
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hIcon = LoadIcon(0, IDI_APPLICATION);
@@ -53,21 +47,21 @@ bool Application::InitMainWindow(HINSTANCE hInstance, WNDPROC wndProc)
 	}
 
 	// Computewindow rectangle dimensions based on requested client area dimensions.
-	RECT rc = { 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT };
+	RECT rc = { 0, 0, m_windowWidth, m_windowHeight };
 	AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, false);
 
 
-	m_mainWindow = CreateWindow(g_WindowClass,       // Window class to use
-								g_Title,             // Tile
-								WS_OVERLAPPEDWINDOW, // Style
-								0,                   // X
-								0,                   // Y
-								rc.right - rc.left,  // Width
-								rc.top - rc.bottom,  // Height
-								nullptr,             // Parent Window
-								nullptr,             // Menu
-								hInstance,           // Application Instance
-								0);                  // Param
+	m_mainWindow = CreateWindow(m_windowClassName.c_str(),  // Window class to use
+								m_appTile.c_str(),          // Tile
+								WS_OVERLAPPEDWINDOW,		// Style
+								0,							// X
+								0,							// Y
+								rc.right - rc.left,			// Width
+								rc.top - rc.bottom,			// Height
+								nullptr,					// Parent Window
+								nullptr,					// Menu
+								hInstance,					// Application Instance
+								0);							// Param
 	if (!m_mainWindow)
 	{
 		MessageBox(0, L"CreateWindow Failed.", 0, 0);
@@ -95,8 +89,94 @@ bool Application::InitDirect3D()
 		D3D12CreateDevice(pWarpAdapter.Get(), D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&m_d3dDevice));
 	}
 
-	return false;
+	m_d3dDevice->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&m_fence));
+
+	// Set render quality
+	D3D12_FEATURE_DATA_MULTISAMPLE_QUALITY_LEVELS msQualityLevels;
+	msQualityLevels.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	msQualityLevels.SampleCount = 4;
+	msQualityLevels.Flags = D3D12_MULTISAMPLE_QUALITY_LEVELS_FLAG_NONE;
+	msQualityLevels.NumQualityLevels = 0;
+	m_d3dDevice->CheckFeatureSupport(D3D12_FEATURE_MULTISAMPLE_QUALITY_LEVELS,
+									 &msQualityLevels,
+									 sizeof(msQualityLevels));
+
+	m_4xMsaaQuality = msQualityLevels.NumQualityLevels;
+	assert(m_4xMsaaQuality > 0 && "Unexpected MSAA quality level.");
+
+	CreateCommandObjects();
+	CreateSwapChain();
+	CreateRtvAndDsvDescriptorHeaps();
+
+	return true;
 }
+
+void Application::CreateCommandObjects()
+{
+	// Create command queue
+	D3D12_COMMAND_QUEUE_DESC queueDesc = {};
+	queueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+	queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+	m_d3dDevice->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&m_commandQueue));
+
+	// Create command allocator
+	m_d3dDevice->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(m_directCmdListAlloc.GetAddressOf()));
+
+	// Start off in a closed state. This is because the first time we refer 
+	// to the command list we will Reset it, and it needs to be closed before
+	// calling Reset.
+	m_commandList->Close();
+}
+
+void Application::CreateSwapChain()
+{
+	// Release the previous swapchain we will be recreating.
+	m_swapChain.Reset();
+
+	DXGI_SWAP_CHAIN_DESC sd;
+	sd.BufferDesc.Width = m_windowWidth;
+	sd.BufferDesc.Height = m_windowHeight;
+	sd.BufferDesc.RefreshRate.Numerator = 60;
+	sd.BufferDesc.RefreshRate.Denominator = 1;
+	sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	sd.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+	sd.SampleDesc.Count = m_4xMsaaState ? 4 : 1;
+	sd.SampleDesc.Quality = m_4xMsaaState ? (m_4xMsaaQuality - 1) : 0;
+	sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	sd.BufferCount = SwapChainBufferCount;
+	sd.OutputWindow = m_mainWindow;
+	sd.Windowed = true;
+	sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	sd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+
+	// Note: Swap chain uses queue to preform flush.
+	m_dxgiFactory->CreateSwapChain(m_commandQueue.Get(), &sd, m_swapChain.GetAddressOf());
+}
+
+void Application::CreateRtvAndDsvDescriptorHeaps()
+{
+	m_rtvDescriptorSize = m_d3dDevice->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+	m_dsvDescriptorSize = m_d3dDevice->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_DSV);
+	m_cbvSrvUavDescriptorSize = m_d3dDevice->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
+
+	D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc;
+	rtvHeapDesc.NumDescriptors = SwapChainBufferCount;
+	rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+	rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+	rtvHeapDesc.NodeMask = 0;
+	m_d3dDevice->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(m_rtvHeap.GetAddressOf()));
+
+	D3D12_DESCRIPTOR_HEAP_DESC dsvHeapDesc;
+	dsvHeapDesc.NumDescriptors = 1;
+	dsvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_DSV;
+	dsvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+	dsvHeapDesc.NodeMask = 0;
+	m_d3dDevice->CreateDescriptorHeap(&dsvHeapDesc, IID_PPV_ARGS(m_dsvHeap.GetAddressOf()));
+
+}
+
+#pragma endregion
+
 
 void Application::Update(float deltaTime)
 {
--- CharactorAnimation/Chapter2/Application.h
+++ CharactorAnimation/Chapter2/Application.h
@@ -4,6 +4,8 @@
 #include <wrl.h>
 #include <dxgi1_4.h>
 #include <d3d12.h>
+#include <cassert>
+#include <string>
 
 // Link necessary d3d12 libraries.
 #pragma comment(lib, "d3dcompiler.lib")
@@ -17,11 +19,17 @@ class Application
 public:
 	Application();
 	~Application();
+
 	HRESULT Init(HINSTANCE hInstance, bool windowed, WNDPROC wndProc);
 	bool InitMainWindow(HINSTANCE hInstance, WNDPROC wndProc);
 	bool InitDirect3D();
+	void CreateCommandObjects();
+	void CreateSwapChain();
+	void CreateRtvAndDsvDescriptorHeaps();
+
 	void Update(float deltaTime);
 	void Render();
+
 	void Cleanup();
 	void Quit();
 
@@ -29,8 +37,35 @@ public:
 	void DeviceGained();
 
 private:
+	static const int SwapChainBufferCount = 2;
+
 	HWND m_mainWindow;
-	
+
+	// Set true to use 4X MSAA. The default is false
+	bool m_4xMsaaState = false; // 4X MSAA enabled
+	UINT m_4xMsaaQuality = 0;   // quality level of 4X MSAA
+
 	Microsoft::WRL::ComPtr<ID3D12Device> m_d3dDevice;
+	Microsoft::WRL::ComPtr<IDXGISwapChain> m_swapChain;
 	Microsoft::WRL::ComPtr<IDXGIFactory4> m_dxgiFactory;
+
+	Microsoft::WRL::ComPtr<ID3D12Fence> m_fence;
+	UINT64 m_currentFence = 0;
+
+	Microsoft::WRL::ComPtr<ID3D12CommandQueue> m_commandQueue;
+	Microsoft::WRL::ComPtr<ID3D12CommandAllocator> m_directCmdListAlloc;
+	Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> m_commandList;
+
+	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> m_rtvHeap;
+	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> m_dsvHeap;
+
+	UINT m_rtvDescriptorSize = 0;
+	UINT m_dsvDescriptorSize = 0;
+	UINT m_cbvSrvUavDescriptorSize = 0;
+
+	// Derived class should set these in derived constructor to customize starting values.
+	std::wstring m_appTile = L"Charactor Animation";
+	std::wstring m_windowClassName = L"MainWnd";
+	int m_windowWidth = 800;
+	int m_windowHeight = 600;
 };
