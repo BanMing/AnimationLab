cf36e2eee7c32ad040c056dd0120bd0a6a12adf6
BanMing <ban-ming@foxmail.com>
2021-08-26 00:40:36 +0800

    add mesh structure

--- CharactorAnimation/Chapter2/Mesh.h
+++ CharactorAnimation/Chapter2/Mesh.h
@@ -1,34 +1,81 @@
 /******************************************************************
 ** Mesh.h
-** @Author       : BanMing 
+** @Author       : BanMing
 ** @Date         : 8/21/2021 11:05:17 PM
-** @Description  : 
+** @Description  :
 *******************************************************************/
 
 #pragma once
 
+#include <wrl.h>
+#include <DirectXCollision.h>
 #include <d3d12.h>
+#include <string>
+#include <unordered_map>
 
-class Mesh
+// Defines a subrange of geometry in a Mesh.This is for when multipe
+// geometries are stored in one vertex and index buffer. It provides the offsets
+// and data needed to draw a subset of geometry stores in the vertex and index
+// buffers so that we can implement the technique.
+struct SubMesh
 {
-public:
-	Mesh();
-	Mesh(char fileName[]);
-	~Mesh();
-	HRESULT Load(char fileName[]);
-	void Render();
-	void Release();
+	UINT IndexCount =0;
+	UINT StartIndexLocation = 0;
+	INT BaseVertexLocation = 0;
 
-private:
-	//ID3D12Mesh
-
-
+	// Bounding box of the geometry defined by this submesh
+	DirectX::BoundingBox Bounds;
 };
 
-Mesh::Mesh()
+struct Mesh
 {
-}
+	// define the mesh
+	std::string Name;
 
-Mesh::~Mesh()
-{
-}
+	// Use Blobs because the vertex/index format can be generic.
+	// It is up to the client to cast appropriately
+	// ID3DBlob is used to return data of arbitrary length
+	Microsoft::WRL::ComPtr<ID3DBlob> VertexBufferCPU = nullptr;
+	Microsoft::WRL::ComPtr<ID3DBlob> IndexBufferCPU = nullptr;
+
+	Microsoft::WRL::ComPtr<ID3D12Resource> VertexBufferGPU = nullptr;
+	Microsoft::WRL::ComPtr<ID3D12Resource> IndexBufferGPU = nullptr;
+
+	Microsoft::WRL::ComPtr<ID3D12Resource> VertexBufferUploader = nullptr;
+	Microsoft::WRL::ComPtr<ID3D12Resource> IndexBufferUploader = nullptr;
+
+	// Data about the buffers.
+	UINT VertexByteStride = 0;
+	UINT VertexBufferByteSize = 0;
+	DXGI_FORMAT IndexFormat = DXGI_FORMAT_R16_UINT;
+	UINT IndexBufferByteSize = 0;
+
+	std::unordered_map<std::string, SubMesh> DrawArgs;
+
+	D3D12_VERTEX_BUFFER_VIEW VertexBufferView()const
+	{
+		D3D12_VERTEX_BUFFER_VIEW vbv;
+		vbv.BufferLocation = VertexBufferGPU->GetGPUVirtualAddress();
+		vbv.StrideInBytes = VertexByteStride;
+		vbv.SizeInBytes = VertexBufferByteSize;
+
+		return vbv;
+	}
+
+	D3D12_INDEX_BUFFER_VIEW IndexBufferView()const
+	{
+		D3D12_INDEX_BUFFER_VIEW ibv;
+		ibv.BufferLocation = IndexBufferGPU->GetGPUVirtualAddress();
+		ibv.Format = IndexFormat;
+		ibv.SizeInBytes = IndexBufferByteSize;
+
+		return ibv;
+	}
+
+	// We can free this memory after we finish upload to the GPU.
+	void DisposeUploaders()
+	{
+		VertexBufferUploader = nullptr;
+		IndexBufferUploader = nullptr;
+	}
+};
