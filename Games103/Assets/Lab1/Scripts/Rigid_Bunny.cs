0ffd5b7b7fb00d855108f97732b52e8dca859f28
BanMing <ban-ming@foxmail.com>
2021-12-16 22:57:35 +0800

    first hw

---
+++ Games103/Assets/Lab1/Scripts/Rigid_Bunny.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rigid_Bunny : MonoBehaviour 
+{
+	bool launched 		= false;
+	float dt 			= 0.015f;
+	Vector3 v 			= new Vector3(0, 0, 0);	// velocity
+	Vector3 w 			= new Vector3(0, 0, 0);	// angular velocity
+	
+	float mass;									// mass
+	Matrix4x4 I_ref;							// reference inertia
+
+	float linear_decay	= 0.999f;				// for velocity decay
+	float angular_decay	= 0.98f;				
+	float restitution 	= 0.5f;					// for collision
+
+
+	// Use this for initialization
+	void Start () 
+	{		
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+		Vector3[] vertices = mesh.vertices;
+
+		float m=1;
+		mass=0;
+		for (int i=0; i<vertices.Length; i++) 
+		{
+			mass += m;
+			float diag=m*vertices[i].sqrMagnitude;
+			I_ref[0, 0]+=diag;
+			I_ref[1, 1]+=diag;
+			I_ref[2, 2]+=diag;
+			I_ref[0, 0]-=m*vertices[i][0]*vertices[i][0];
+			I_ref[0, 1]-=m*vertices[i][0]*vertices[i][1];
+			I_ref[0, 2]-=m*vertices[i][0]*vertices[i][2];
+			I_ref[1, 0]-=m*vertices[i][1]*vertices[i][0];
+			I_ref[1, 1]-=m*vertices[i][1]*vertices[i][1];
+			I_ref[1, 2]-=m*vertices[i][1]*vertices[i][2];
+			I_ref[2, 0]-=m*vertices[i][2]*vertices[i][0];
+			I_ref[2, 1]-=m*vertices[i][2]*vertices[i][1];
+			I_ref[2, 2]-=m*vertices[i][2]*vertices[i][2];
+		}
+		I_ref [3, 3] = 1;
+	}
+	
+	Matrix4x4 Get_Cross_Matrix(Vector3 a)
+	{
+		//Get the cross product matrix of vector a
+		Matrix4x4 A = Matrix4x4.zero;
+		A [0, 0] = 0; 
+		A [0, 1] = -a [2]; 
+		A [0, 2] = a [1]; 
+		A [1, 0] = a [2]; 
+		A [1, 1] = 0; 
+		A [1, 2] = -a [0]; 
+		A [2, 0] = -a [1]; 
+		A [2, 1] = a [0]; 
+		A [2, 2] = 0; 
+		A [3, 3] = 1;
+		return A;
+	}
+
+	// In this function, update v and w by the impulse due to the collision with
+	//a plane <P, N>
+	void Collision_Impulse(Vector3 P, Vector3 N)
+	{
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		//Game Control
+		if(Input.GetKey("r"))
+		{
+			transform.position = new Vector3 (0, 0.6f, 0);
+			restitution = 0.5f;
+			launched=false;
+		}
+		if(Input.GetKey("l"))
+		{
+			v = new Vector3 (5, 2, 0);
+			launched=true;
+		}
+
+		// Part I: Update velocities
+
+
+		// Part II: Collision Impulse
+		Collision_Impulse(new Vector3(0, 0.01f, 0), new Vector3(0, 1, 0));
+		Collision_Impulse(new Vector3(2, 0, 0), new Vector3(-1, 0, 0));
+
+		// Part III: Update position & orientation
+		//Update linear status
+		Vector3 x    = transform.position;
+		//Update angular status
+		Quaternion q = transform.rotation;
+
+		// Part IV: Assign to the object
+		transform.position = x;
+		transform.rotation = q;
+	}
+}
