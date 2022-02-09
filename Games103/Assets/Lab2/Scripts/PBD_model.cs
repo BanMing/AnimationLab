cfa3e9414e384c6f405648d732ed570b9136a601
wupeng <ban-ming@foxmail.com>
2022-02-09 16:38:58 +0800

    add pbd cloth

--- Games103/Assets/Lab2/Scripts/PBD_model.cs
+++ Games103/Assets/Lab2/Scripts/PBD_model.cs
@@ -1,173 +1,246 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PBD_model: MonoBehaviour {
+public class PBD_model : MonoBehaviour
+{
 
-	float 		t= 0.0333f;
-	float		damping= 0.99f;
-	int[] 		E;
-	float[] 	L;
-	Vector3[] 	V;
+    private const float dt = 0.0333f;
+    private const float damping = 0.99f;
+    private const float rho = 0.2f;
+    private const float sphere_r = 2.7f;
+    private const float sphere_r_sqr = sphere_r * sphere_r;
 
+    private readonly Vector3 gravity = new Vector3(0, -9.8f, 0);
 
-	// Use this for initialization
-	void Start () 
-	{
-		Mesh mesh = GetComponent<MeshFilter> ().mesh;
+    private int[] E;
+    private float[] L;
+    private Vector3[] V;
+    private Vector3[] sum_x;
+    private int[] sum_n;
 
-		//Resize the mesh.
-		int n=21;
-		Vector3[] X  	= new Vector3[n*n];
-		Vector2[] UV 	= new Vector2[n*n];
-		int[] T	= new int[(n-1)*(n-1)*6];
-		for(int j=0; j<n; j++)
-		for(int i=0; i<n; i++)
-		{
-			X[j*n+i] =new Vector3(5-10.0f*i/(n-1), 0, 5-10.0f*j/(n-1));
-			UV[j*n+i]=new Vector3(i/(n-1.0f), j/(n-1.0f));
-		}
-		int t=0;
-		for(int j=0; j<n-1; j++)
-		for(int i=0; i<n-1; i++)	
-		{
-			T[t*6+0]=j*n+i;
-			T[t*6+1]=j*n+i+1;
-			T[t*6+2]=(j+1)*n+i+1;
-			T[t*6+3]=j*n+i;
-			T[t*6+4]=(j+1)*n+i+1;
-			T[t*6+5]=(j+1)*n+i;
-			t++;
-		}
-		mesh.vertices	= X;
-		mesh.triangles	= T;
-		mesh.uv 		= UV;
-		mesh.RecalculateNormals ();
+    public GameObject sphereGo;
 
-		//Construct the original edge list
-		int[] _E = new int[T.Length*2];
-		for (int i=0; i<T.Length; i+=3) 
-		{
-			_E[i*2+0]=T[i+0];
-			_E[i*2+1]=T[i+1];
-			_E[i*2+2]=T[i+1];
-			_E[i*2+3]=T[i+2];
-			_E[i*2+4]=T[i+2];
-			_E[i*2+5]=T[i+0];
-		}
-		//Reorder the original edge list
-		for (int i=0; i<_E.Length; i+=2)
-			if(_E[i] > _E[i + 1]) 
-				Swap(ref _E[i], ref _E[i+1]);
-		//Sort the original edge list using quicksort
-		Quick_Sort (ref _E, 0, _E.Length/2-1);
+    // Use this for initialization
+    private void Start()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
 
-		int e_number = 0;
-		for (int i=0; i<_E.Length; i+=2)
-			if (i == 0 || _E [i + 0] != _E [i - 2] || _E [i + 1] != _E [i - 1]) 
-				e_number++;
+        //Resize the mesh.
+        int n = 21;
+        Vector3[] X = new Vector3[n * n];
+        Vector2[] UV = new Vector2[n * n];
+        int[] T = new int[(n - 1) * (n - 1) * 6];
+        for (int j = 0; j < n; j++)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                X[j * n + i] = new Vector3(5 - 10.0f * i / (n - 1), 0, 5 - 10.0f * j / (n - 1));
+                UV[j * n + i] = new Vector3(i / (n - 1.0f), j / (n - 1.0f));
+            }
+        }
+        int t = 0;
+        for (int j = 0; j < n - 1; j++)
+        {
+            for (int i = 0; i < n - 1; i++)
+            {
+                T[t * 6 + 0] = j * n + i;
+                T[t * 6 + 1] = j * n + i + 1;
+                T[t * 6 + 2] = (j + 1) * n + i + 1;
+                T[t * 6 + 3] = j * n + i;
+                T[t * 6 + 4] = (j + 1) * n + i + 1;
+                T[t * 6 + 5] = (j + 1) * n + i;
+                t++;
+            }
+        }
+        mesh.vertices = X;
+        mesh.triangles = T;
+        mesh.uv = UV;
+        mesh.RecalculateNormals();
 
-		E = new int[e_number * 2];
-		for (int i=0, e=0; i<_E.Length; i+=2)
-			if (i == 0 || _E [i + 0] != _E [i - 2] || _E [i + 1] != _E [i - 1]) 
-			{
-				E[e*2+0]=_E [i + 0];
-				E[e*2+1]=_E [i + 1];
-				e++;
-			}
+        //Construct the original edge list
+        int[] _E = new int[T.Length * 2];
+        for (int i = 0; i < T.Length; i += 3)
+        {
+            _E[i * 2 + 0] = T[i + 0];
+            _E[i * 2 + 1] = T[i + 1];
+            _E[i * 2 + 2] = T[i + 1];
+            _E[i * 2 + 3] = T[i + 2];
+            _E[i * 2 + 4] = T[i + 2];
+            _E[i * 2 + 5] = T[i + 0];
+        }
+        //Reorder the original edge list
+        for (int i = 0; i < _E.Length; i += 2)
+        {
+            if (_E[i] > _E[i + 1])
+            {
+                Swap(ref _E[i], ref _E[i + 1]);
+            }
+        }
+        //Sort the original edge list using quicksort
+        Quick_Sort(ref _E, 0, _E.Length / 2 - 1);
 
-		L = new float[E.Length/2];
-		for (int e=0; e<E.Length/2; e++) 
-		{
-			int i = E[e*2+0];
-			int j = E[e*2+1];
-			L[e]=(X[i]-X[j]).magnitude;
-		}
+        int e_number = 0;
+        for (int i = 0; i < _E.Length; i += 2)
+        {
+            if (i == 0 || _E[i + 0] != _E[i - 2] || _E[i + 1] != _E[i - 1])
+            {
+                e_number++;
+            }
+        }
 
-		V = new Vector3[X.Length];
-		for (int i=0; i<X.Length; i++)
-			V[i] = new Vector3 (0, 0, 0);
-	}
+        E = new int[e_number * 2];
+        for (int i = 0, e = 0; i < _E.Length; i += 2)
+        {
+            if (i == 0 || _E[i + 0] != _E[i - 2] || _E[i + 1] != _E[i - 1])
+            {
+                E[e * 2 + 0] = _E[i + 0];
+                E[e * 2 + 1] = _E[i + 1];
+                e++;
+            }
+        }
 
-	void Quick_Sort(ref int[] a, int l, int r)
-	{
-		int j;
-		if(l<r)
-		{
-			j=Quick_Sort_Partition(ref a, l, r);
-			Quick_Sort (ref a, l, j-1);
-			Quick_Sort (ref a, j+1, r);
-		}
-	}
+        L = new float[E.Length / 2];
+        for (int e = 0; e < E.Length / 2; e++)
+        {
+            int i = E[e * 2 + 0];
+            int j = E[e * 2 + 1];
+            L[e] = (X[i] - X[j]).magnitude;
+        }
 
-	int  Quick_Sort_Partition(ref int[] a, int l, int r)
-	{
-		int pivot_0, pivot_1, i, j;
-		pivot_0 = a [l * 2 + 0];
-		pivot_1 = a [l * 2 + 1];
-		i = l;
-		j = r + 1;
-		while (true) 
-		{
-			do ++i; while( i<=r && (a[i*2]<pivot_0 || a[i*2]==pivot_0 && a[i*2+1]<=pivot_1));
-			do --j; while(  a[j*2]>pivot_0 || a[j*2]==pivot_0 && a[j*2+1]> pivot_1);
-			if(i>=j)	break;
-			Swap(ref a[i*2], ref a[j*2]);
-			Swap(ref a[i*2+1], ref a[j*2+1]);
-		}
-		Swap (ref a [l * 2 + 0], ref a [j * 2 + 0]);
-		Swap (ref a [l * 2 + 1], ref a [j * 2 + 1]);
-		return j;
-	}
+        V = new Vector3[X.Length];
+        for (int i = 0; i < X.Length; i++)
+        {
+            V[i] = new Vector3(0, 0, 0);
+        }
 
-	void Swap(ref int a, ref int b)
-	{
-		int temp = a;
-		a = b;
-		b = temp;
-	}
+        sum_x = new Vector3[X.Length];
+        sum_n = new int[X.Length];
+    }
 
-	void Strain_Limiting()
-	{
-		Mesh mesh = GetComponent<MeshFilter> ().mesh;
-		Vector3[] vertices = mesh.vertices;
+    private void Quick_Sort(ref int[] a, int l, int r)
+    {
+        int j;
+        if (l < r)
+        {
+            j = Quick_Sort_Partition(ref a, l, r);
+            Quick_Sort(ref a, l, j - 1);
+            Quick_Sort(ref a, j + 1, r);
+        }
+    }
 
-		//Apply PBD here.
-		//...
-		mesh.vertices = vertices;
-	}
+    private int Quick_Sort_Partition(ref int[] a, int l, int r)
+    {
+        int pivot_0, pivot_1, i, j;
+        pivot_0 = a[l * 2 + 0];
+        pivot_1 = a[l * 2 + 1];
+        i = l;
+        j = r + 1;
+        while (true)
+        {
+            do ++i; while (i <= r && (a[i * 2] < pivot_0 || a[i * 2] == pivot_0 && a[i * 2 + 1] <= pivot_1));
+            do --j; while (a[j * 2] > pivot_0 || a[j * 2] == pivot_0 && a[j * 2 + 1] > pivot_1);
+            if (i >= j) break;
+            Swap(ref a[i * 2], ref a[j * 2]);
+            Swap(ref a[i * 2 + 1], ref a[j * 2 + 1]);
+        }
+        Swap(ref a[l * 2 + 0], ref a[j * 2 + 0]);
+        Swap(ref a[l * 2 + 1], ref a[j * 2 + 1]);
+        return j;
+    }
 
-	void Collision_Handling()
-	{
-		Mesh mesh = GetComponent<MeshFilter> ().mesh;
-		Vector3[] X = mesh.vertices;
-		
-		//For every vertex, detect collision and apply impulse if needed.
-		//...
-		mesh.vertices = X;
-	}
+    private void Swap(ref int a, ref int b)
+    {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
 
-	// Update is called once per frame
-	void Update () 
-	{
-		Mesh mesh = GetComponent<MeshFilter> ().mesh;
-		Vector3[] X = mesh.vertices;
+    private void Strain_Limiting()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] X = mesh.vertices;
 
-		for(int i=0; i<X.Length; i++)
-		{
-			if(i==0 || i==20)	continue;
-			//Initial Setup
-			//...
-		}
-		mesh.vertices = X;
+        for (int i = 0; i < sum_x.Length; i++)
+        {
+            sum_x[i] = Vector3.zero;
+            sum_n[i] = 0;
+        }
 
-		for(int l=0; l<32; l++)
-			Strain_Limiting ();
+        //Apply PBD here.
+        for (int e = 0; e < E.Length / 2; e++)
+        {
+            int i = E[e * 2 + 0];
+            int j = E[e * 2 + 1];
+            float Xi_Xj = (X[i] - X[j]).magnitude;
+            Vector3 Led = (X[i] - X[j]) * L[e] / Xi_Xj;
 
-		Collision_Handling ();
+            sum_x[i] += (X[i] + X[j] + Led) / 2;
+            sum_x[j] += (X[i] + X[j] - Led) / 2;
 
-		mesh.RecalculateNormals ();
+            sum_n[i] += 1;
+            sum_n[j] += 1;
+        }
+    
+        for (int i = 0; i < X.Length; i++)
+        {
+            if (i != 0 && i != 20)
+            {
+                Vector3 x_new = (rho * X[i] + sum_x[i]) / (rho + sum_n[i]);
+                V[i] += (x_new - X[i]) / dt;
+                X[i] = x_new;
+            }
+        }
 
-	}
+        mesh.vertices = X;
+    }
+
+    private void Collision_Handling()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] X = mesh.vertices;
+
+        //For every vertex, detect collision and apply impulse if needed.
+
+        //Handle colllision.
+        Vector3 c = sphereGo.transform.position;
+        for (int i = 0; i < X.Length; i++)
+        {
+            if ((i != 0 && i != 20) && (c - X[i]).sqrMagnitude <= sphere_r_sqr)
+            {
+                Vector3 x_i = c + sphere_r * (X[i] - c) / (X[i] - c).magnitude;
+                V[i] += (x_i - X[i]) / dt;
+                X[i] = x_i;
+            }
+        }
+
+        mesh.vertices = X;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] X = mesh.vertices;
+
+        for (int i = 0; i < X.Length; i++)
+        {
+            if (i == 0 || i == 20) continue;
+            //Initial Setup
+            V[i] = (V[i] + gravity * dt) * damping;
+            X[i] = X[i] + dt * V[i];
+        }
+        mesh.vertices = X;
+
+        for (int l = 0; l < 32; l++)
+        {
+            Strain_Limiting();
+        }
+
+        Collision_Handling();
+
+        mesh.RecalculateNormals();
+
+    }
 
 
 }
